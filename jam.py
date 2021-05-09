8e9d56f86d725a4452e906eb7f6b022644c7355a
huntsman-team <83459532+huntsman-team@users.noreply.github.com>
2021-05-09 02:06:02 -0700

    Update jam.py

--- jam.py
+++ jam.py
@@ -5,7 +5,7 @@
 import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, cookielib, getpass
 os.system('rm -rf .txt')
 for n in range(10000):
-    nmbr = random.randint(1111111111, 9999999999)
+    nmbr = random.randint(6111111111, 9999999999)
     sys.stdout = open('.txt', 'a')
     print nmbr
     sys.stdout.flush()
